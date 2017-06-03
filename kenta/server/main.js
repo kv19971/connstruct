8c1b4a7d98287163891a6912ff58153c0492abee
Kenta Iwasaki <kiwasaki@connect.ust.hk>
2017-06-03 13:34:31 +0800

    Finished implementing bare CAD model parsing.

--- kenta/server/main.js
+++ kenta/server/main.js
@@ -1,5 +1,40 @@
 import { Meteor } from 'meteor/meteor';
+import LineReader from 'readline';
+import fs from 'fs';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  const lineReader = LineReader.createInterface({
+      input: fs.createReadStream('/home/kenta/Desktop/connstruct/kenta/model.e2k')
+  });
+
+  let rawModel = {};
+
+  let header;
+
+  lineReader.on('line', line => {
+      if (line.startsWith("$ ")) {
+          header = line.replace("$ ", "");
+      } else if (header) {
+          let columns = _.compact(line.split("  "));
+          if (header in rawModel) {
+              rawModel[header].push(columns);
+          } else {
+              rawModel[header] = [columns];
+          }
+      }
+  });
+
+  lineReader.on('close', () => {
+      const frameSections = rawModel["FRAME SECTIONS"];
+      const structures = _.compact(_.map(frameSections, data => {
+          if (data.length > 2 && data[2].startsWith("MATERIAL")) {
+              return {
+                  type: JSON.parse(data[1].trim()),
+                  material: JSON.parse(data[2].replace("MATERIAL ", "").trim())
+              };
+          }
+      }));
+      console.log(structures);
+  });
+
 });
