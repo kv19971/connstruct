5d1a7d9032306958511c5945d42f7d554be908b3
v97 <vsambamurthy@connect.ust.hk>
2017-06-04 07:28:31 +0800

    Finished basic diagram of structural components behind model.

--- demo/client/importcad/importcad.js
+++ demo/client/importcad/importcad.js
@@ -8,7 +8,7 @@ OBJLoader(three)
 import './importcad.html';
 var width = 900;
 const height = 550;
-var pxr = width/height;
+var pxr = width / height;
 //obj = require('./obj/building.obj');
 var angle = 0;
 Template.importcad.viewmodel({
@@ -16,65 +16,65 @@ Template.importcad.viewmodel({
     signal: 'mouse',
     onCreated() {
         console.log(document);
-        
-        
+
 
     },
     onRendered(){
-        width =  $("#mainuishit").width() - 30;
+        width = $("#mainuishit").width() - 30;
         var texture = new three.Texture();
 
-        var loader = new three.ImageLoader( manager );
-				loader.load( 'txt.jpg', function ( image ) {
-					texture.image = image;
-					texture.needsUpdate = true;
-				} );
+        var loader = new three.ImageLoader(manager);
+        loader.load('txt.jpg', function (image) {
+            texture.image = image;
+            texture.needsUpdate = true;
+        });
 
-        var camera = new three.PerspectiveCamera(45, width / height, 1, 2000 );
+        var camera = new three.PerspectiveCamera(45, width / height, 1, 2000);
         camera.position.z = 15;
-        
+
         var scene = new three.Scene();
 
-        var ambient = new three.AmbientLight( 0xdddddd );
-				scene.add( ambient );
-        var pointLight = new three.PointLight( 0xffffff, 1 );
+        var ambient = new three.AmbientLight(0xdddddd);
+        scene.add(ambient);
+        var pointLight = new three.PointLight(0xffffff, 1);
         scene.add(pointLight);
-        
+
 
         var manager = new three.LoadingManager();
         var loaders = new three.OBJLoader(manager);
-        loaders.load("building.obj", function ( object ) {
-                object.position.x = 5;
-                    object.position.y = 0;
-					scene.add( object );
-				});
+        loaders.load("building.obj", function (object) {
+            object.position.x = 5;
+            object.position.y = 0;
+            scene.add(object);
+        });
         var renderer = new three.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( width, height );
-controls = new OrbitControls( camera, renderer.domElement );
-				controls.addEventListener( 'change', render ); // remove when using animation loop
-				// enable animation loop when using damping or autorotation
-				//controls.enableDamping = true;
-				//controls.dampingFactor = 0.25;
-				controls.enableZoom = false;
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(width, height);
+        controls = new OrbitControls(camera, renderer.domElement);
+        controls.addEventListener('change', render); // remove when using animation loop
+        // enable animation loop when using damping or autorotation
+        //controls.enableDamping = true;
+        //controls.dampingFactor = 0.25;
+        controls.enableZoom = false;
         document.getElementById("model_cad").appendChild(renderer.domElement);
-renderer.physicallyBasedShading = true;
-animate();
-      setInterval(function(){
-        angle = (angle + 0.05) % 360; 
-      }, 50)
-      function render() {
-        let radius = 15;
-        camera.position.x = radius * Math.cos( angle );  
-        camera.position.z = radius * Math.sin( angle );
-				camera.lookAt( scene.position );
-               // requestAnimationFrame(render);
-				renderer.render( scene, camera );
-			}
-      function animate() {
-				requestAnimationFrame( animate );
-				render();
-			}
-      
+        renderer.physicallyBasedShading = true;
+        animate();
+        setInterval(function () {
+            angle = (angle + 0.05) % 360;
+        }, 50)
+        function render() {
+            let radius = 15;
+            camera.position.x = radius * Math.cos(angle);
+            camera.position.z = radius * Math.sin(angle);
+            camera.lookAt(scene.position);
+            // requestAnimationFrame(render);
+            renderer.render(scene, camera);
+        }
+
+        function animate() {
+            requestAnimationFrame(animate);
+            render();
+        }
+
     }
 });
--- demo/client/suppliers/suppliers.js
+++ demo/client/suppliers/suppliers.js
@@ -1,35 +1,86 @@
 /**
  * Created by kenta on 6/3/17.
  */
-var jsnx = require("jsnetworkx");
+let jsnx = require("jsnetworkx");
 Template.suppliers.viewmodel({
     onCreated() {
         console.log("On suppliers page.");
     },
     onRendered() {
-        var G = new jsnx.Graph();
+        let G = new jsnx.Graph();
 
-        G.addNodesFrom([
-                [1, {color: 'red', name:"Supplier A"}],
-                [2, {color: 'green', name:"Supplier B"}],
-                [3, {color: 'white', name: "Supplier C"}]
-            ]);
-            
-            G.addEdgesFrom([[1,2], [1,3]]);
-            
-            // `jsnx.draw` accept a graph and configuration object
-            jsnx.draw(G, {
-            element: '#canvas',
-            withLabels: true,
-            nodeStyle: {
-                fill: function(d) {
-                    return d.data.color;
-                }
-            },
-            nodeAttr: {
-                r: 10,
-                title: function(d) { return d.data.name;}
+        Meteor.call('model.materials', (err, res) => {
+            if (!err) {
+                const materials = res;
+
+                Meteor.call('materials.get', materials, 0.001, 39.768377, -86.158042,
+                    1, 99999999999999, (err, res) => {
+                        if (err) {
+                            alert(err);
+                        } else {
+
+                            let col = d3.scale.linear()
+                                .domain([0, 0.5, 1])
+                                .range(['red', 'orange', 'green']);
+
+                            let nodes = [
+                                ["Center", {
+                                    color: 'white', name: "center", toString: function () {
+                                        return "center";
+                                    }
+                                }]
+                                // [1, {color: 'red', name:"A"}],
+                                // [2, {color: 'green', name:"B"}],
+                                // [3, {color: 'white', name: "C"}]
+                            ];
+
+                            let edges = []
+                            let materialIndex = 0;
+                            _.each(res, material => {
+                                nodes.push([materials[materialIndex], {color: 'blue', name: materials[materialIndex]}]);
+                                edges.push(["Center", materials[materialIndex]])
+                                _.each(material, supplier => {
+                                    nodes.push([supplier.supplier.name, {
+                                        color: col(supplier.rating),
+                                        name: supplier.supplier.name
+                                    }]);
+                                    edges.push([materials[materialIndex], supplier.supplier.name]);
+                                })
+                                materialIndex++;
+                            })
+
+                            G.addNodesFrom(nodes);
+                            G.addEdgesFrom(edges);
+
+                            jsnx.draw(G, {
+                                element: '#canvas',
+                                withLabels: true,
+                                nodeStyle: {
+                                    fill: function (d) {
+                                        return d.data.color;
+                                    }
+                                },
+                                nodeShape: 'circle',
+                                nodeAttr: {
+                                    r: 40,
+                                    label: function (d) {
+                                        return d.data.name;
+                                    }
+                                },
+                                labelAttr: {
+                                    // "text-anchor": "middle
+                                },
+                                layoutAttr: {
+                                    linkDistance: 120,
+                                    charge: -5999
+                                },
+                            });
+                        }
+                    });
             }
-            });
+        })
+
+
+        // `jsnx.draw` accept a graph and configuration object
     }
 })
