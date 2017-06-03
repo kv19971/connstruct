c4546b12745a481b807d94c31c66e78414626315
v97 <vsambamurthy@connect.ust.hk>
2017-06-03 16:00:21 +0800

    Added routing -Vikram

--- main/simple-todos/client/main.js
+++ main/simple-todos/client/main.js
@@ -2,3 +2,13 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+
+Template.mainLayout.viewmodel({
+    state : 0,
+    sites : ['info', 'time', 'budget', 'importcad'],
+    x : "pushMyButtons",
+    nextPage : function(e){
+        alert("jews");
+        FlowRouter.go("/info");
+    }
+})
