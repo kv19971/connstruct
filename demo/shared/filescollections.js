b81ea957c4fdf45e90ffd51242dd50ee3a72fc98
Kshitij Verma <kshitijverma999@hotmail.com>
2017-06-04 13:38:28 +0800

    fixed uploading

--- demo/shared/filescollections.js
+++ demo/shared/filescollections.js
@@ -5,11 +5,8 @@ Images = new FilesCollection({
   collectionName: 'Images',
   allowClientCode: false, // Disallow remove files from Client
   onBeforeUpload: function (file) {
-    // Allow upload files under 10MB, and only in png/jpg/jpeg formats
-    if (file.size <= 1024 * 1024 * 10 && /png|jpe?g/i.test(file.extension)) {
-      return true;
-    }
-    return 'Please upload image, with size equal or less than 10MB';
+    return true;
+    
   }
 });
 
